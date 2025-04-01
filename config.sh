7f8341ac0ef4bb6754d0caa6dc40bf6032772628
Igor Botukhov <igor.botukhov@gmail.com>
2025-04-01 23:53:51 +0000

    Added disks config

--- config.sh
+++ config.sh
@@ -1,6 +1,10 @@
 SETUP_USER_NAME=""
 SETUP_USER_PASSWORD=""
+
 SETUP_DISTRO="void"
+
+SETUP_DISK_SIZE_EFI="512MB"
+SETUP_DISK_SIZE_LUKS=""
 
 # Here you need to write `true` to setup.sh has started execution
 SETUP_START=false
